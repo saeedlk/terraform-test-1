715efd63a465d3207560e5747e5b07b1dfefe8a1
Saeid <98920239+saeedlk@users.noreply.github.com>
2022-11-20 16:56:21 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-resource "aws_ssm_parameters" "foo" {
-  name = "foo"
-  type = "String"
-  value = "bar"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-lk"
+
+  tags = {
+    Name        = "My bucket lk"
+  }
 }
+
+#resource "aws_s3_bucket_acl" "example" {
+#  bucket = aws_s3_bucket.b.id
+#  acl    = "private"
+#}
